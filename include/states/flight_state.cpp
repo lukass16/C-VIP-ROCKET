c0e32fdfd1d10ba0aed83489f1b3307bec25d208
lukass16 <lukasskellijs@gmail.com>
2021-10-12 22:25:30 +0300

    Update flight_state.cpp

--- include/states/flight_state.cpp
+++ include/states/flight_state.cpp
@@ -23,15 +23,16 @@ private:
         if (magnetometer::isApogee())
         {
             count++;
-            return count > 100;
+            if (count>100)
+            {
+                Serial.println("Magnetometer detected apogee!");
+                return true;
+            }
         }
         else if (magnetometer::timerDetectApogee())
         {
             return true;
-        }
-        else if (gpsDetAp)   //*added gpsDetAp functionality
-        {
-            return true;
+            Serial.println("Timer detected apogee");
         }
 
         return false;
@@ -44,43 +45,31 @@ public:
     {
         Serial.println("proof of concept --- FLIGHT STATE");
 
-
-        //!Ther is danger that if launch was detected previously the rocket goes straight to arming and setting apogee timers 
-        //!so if launch is detected and during testing the launchDetected EEPROM value is not changed it could lead to an inadvertent triggering of the mechanism
-
-        if(magnetometer::hasBeenLaunch())
-        {
-            GPSTimeElapsed = gps::getGPSTimeElapsed(gps::getMinute(), gps::getSecond());
-            if(GPSTimeElapsed > 14)
-            {
-                gpsDetAp = 1;
-            }
-            else
-            {
-                magnetometer::startApogeeTimer((14 - GPSTimeElapsed) * 1000000);
-                timerEnabled = 1;
-            }
-        }
+        //!There is danger that if launch was detected previously the rocket goes straight to arming and setting apogee timers 
+        //!straight to arming and setting apogee timers, so if launch is detected and during testing the launchDetected EEPROM 
+        //!value is not changed it could lead to an inadvertent triggering of the mechanism
 
         while (!isApogee())
         {
 
-
+            buzzer::signalThirdSwitch();
             //While apogee isn't reached and the timer isn't yet enabled the rocket checks for launch to enable the timer - the checking of launch has no other functionality
             if (!timerEnabled)
             {
                 if (magnetometer::launch())  //checks if rocket has been launched
                 {
                     magnetometer::startApogeeTimer(14000000); //code to start timer - prints TIMER ENABLED if timer enabled
+                    Serial.println("Launch detected!");
                     timerEnabled = 1;
                 }
             }
 
             // GPS
             gps::readGps();
+            sens_data::GpsData gd;
             if (gps::hasData)
             {
-                sens_data::GpsData gd = gps::getGpsState();
+                gd = gps::getGpsState();
                 s_data.setGpsData(gd);
             }
 
@@ -90,18 +79,14 @@ public:
             s_data.setMagnetometerData(md);
 
             // BAROMETER
-            barometer::readSensor(); // This is only to display data
             sens_data::BarometerData bd = barometer::getBarometerState();
             s_data.setBarometerData(bd);
 
             Serial.println("Looping in flight state!");
-
-            //TODO Flash
-
-            // delay(1000);
         }
 
         Serial.println("APOGEE DETECTED !!!");
+        arming::nihromActivate();
 
         this->_context->RequestNextPhase();
         this->_context->Start();
