4b758e462115475d14be74ea305483cae7c6de4a
lukass16 <lukasskellijs@gmail.com>
2021-10-12 23:11:12 +0300

    Update lora_wrapper.h

--- include/lora_wrapper.h
+++ include/lora_wrapper.h
@@ -53,9 +53,30 @@ namespace lora {
         Serial.println(LoRa.available());
     }
 
-    void readMessage()
-    {
-        onReceive(LoRa.parsePacket());
+    String readMessage(){
+        int packetSize = LoRa.parsePacket();
+        String message = "";
+        if(packetSize){
+            // read packet header bytes:
+            int recipient = LoRa.read();          // recipient address
+            byte sender = LoRa.read();            // sender address
+            byte incomingMsgId = LoRa.read();     // incoming msg ID
+            byte incomingLength = LoRa.read();    // incoming msg length
+
+            if (recipient != _localAddress && recipient != 0xFF) {
+                Serial.println("This message is not for me, recipient: " + String(recipient));
+                return "NULL";                            
+            }   
+            while(LoRa.available()){
+                message += (char)LoRa.read();
+            }
+            Serial.print("Message: " + message);
+        }
+        else {
+            return "NULL";
+        }
+        Serial.println(" RSSI: " + String(LoRa.packetRssi()));
+        return message;
     }
 
     void sendMessage(String outgoing, int lora_message_id)
@@ -72,51 +93,6 @@ namespace lora {
     int getPacketRssi()
     {
         return LoRa.packetRssi();
-    }
-
-    void onReceive(int packetSize)
-    {
-        if (packetSize == 0)
-            return; // if there's no packet, return
-
-        // read packet header bytes:
-        int recipient = LoRa.read();       // recipient address
-        byte sender = LoRa.read();         // sender address
-        byte incomingMsgId = LoRa.read();  // incoming msg ID
-        byte incomingLength = LoRa.read(); // incoming msg length
-
-        String incoming = "";
-        while (LoRa.available())
-        {
-            incoming += (char)LoRa.read();
-        }
-
-        if (incomingLength != incoming.length())
-        { // check length for error
-            Serial.println("error: message length does not match length");
-            return; // skip rest of function
-        }
-
-        //   if the recipient isn't this device or broadcast,
-        if (recipient != _localAddress && recipient != 0xFF)
-        {
-            Serial.println("This message is not for me.");
-            return; // skip rest of function
-        }
-
-        // if message is for this device, or broadcast, print details:
-        if (_canPrintHeaderMessage)
-        {
-            Serial.println("Received from: 0x" + String(sender, HEX));
-            Serial.println("Sent to: 0x" + String(recipient, HEX));
-            Serial.println("Message ID: " + String(incomingMsgId));
-            Serial.println("Message length: " + String(incomingLength));
-        }
-
-        Serial.println("Message: " + incoming);
-        // Serial.println("RSSI: " + String(LoRa.packetRssi()));
-        // Serial.println("Snr: " + String(LoRa.packetSnr()));
-        Serial.println();
     }
 
     void test()
